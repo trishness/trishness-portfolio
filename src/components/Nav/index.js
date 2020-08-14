4574182906a611ff94aa69db7b137b062049d240
Trish Ness <trishnss@gmail.com>
2020-08-14 09:20:45 -0700

    className in nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,28 +6,28 @@ import resume from "./resume.pdf"
 function Nav() {
     return (
         <>
-            <nav class="nav navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand"><strong>Trish Ness</strong></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="nav navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand"><strong>Trish Ness</strong></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/about">About</Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/portfolio">Portfolio</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/portfolio">Portfolio</Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://github.com/trishness" target="_blank">GitHub</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="https://github.com/trishness" target="_blank">GitHub</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.linkedin.com/in/trish-ness-39593544" target="_blank">LinkedIn</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="https://www.linkedin.com/in/trish-ness-39593544" target="_blank">LinkedIn</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href={resume} target="_blank">Resume</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href={resume} target="_blank">Resume</a>
                         </li>
                     </ul>
                 </div>
