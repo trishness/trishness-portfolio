1cfa4e5631932690a0a5e94e256269e7fc9c7b11
Trish Ness <trishnss@gmail.com>
2020-08-17 17:08:02 -0700

    trying to get the nav bar to show up

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom"
-import style from "./style.css"
+// import style from "./style.css"
 import resume from "./resume.pdf"
 
 function Nav() {
